dfbb928c053151a06c3a31bbeb2baad92ddde9c3
gouravkshirsagar5500 <gouravkshirsagar5500@gmail.com>
2024-05-26 12:45:49 +0530

    vite+react+tailwind starter pack

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1>Vite + React + Tailwind</h1>
+      <h3>This is basic starter pack for react +tailwind</h3>
+      <div className="card">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
